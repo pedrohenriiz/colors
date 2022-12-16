58ca60049bcffc387d991affcc4b361f77a08a17
pedro henrique savarezzi maria <pedro.avare01@gmail.com>
2022-12-15 21:51:24 -0300

    Adds action icons on generated colors

--- src/components/GeneratedColor/index.tsx
+++ src/components/GeneratedColor/index.tsx
@@ -1,3 +1,5 @@
+import { LockOpen, ArrowClockwise, Copy } from 'phosphor-react';
+
 interface GeneratedColorProps {
   color: string;
 }
@@ -5,8 +7,14 @@ interface GeneratedColorProps {
 export function GeneratedColor({ color }: GeneratedColorProps) {
   return (
     <div
-      className='w-full h-16 md:h-80'
+      className='w-full h-16 flex flex-col justify-center items-center md:justify-center md:items-end md:flex-row md:h-80 group'
       style={{ backgroundColor: color }}
-    ></div>
+    >
+      <div className='flex flex-row items-center justify-center space-x-10  sm:group-hover:flex md:flex-col md:space-x-0 md:space-y-10 md:mb-10 lg:hidden '>
+        <ArrowClockwise className='cursor-pointer text-lg sm:text-xl' />
+        <Copy className='cursor-pointer text-lg sm:text-xl' />
+        <LockOpen className='cursor-pointer text-lg sm:text-xl' />
+      </div>
+    </div>
   );
 }
