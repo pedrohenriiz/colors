74b7e95684472e5721424613eb98526336979b59
pedro henrique savarezzi maria <pedro.avare01@gmail.com>
2023-01-10 22:26:22 -0300

    Adds save palette hook

---
+++ src/hooks/useSavePalette.ts
@@ -0,0 +1,66 @@
+import { getPalettes, setPalette } from '../storage/palettes';
+import { ColorProps } from '../App';
+
+type SavePaletteProps = {
+  colors: ColorProps[];
+  edittingCurrentIndex: null | number;
+  isEditting: boolean;
+  setEdittingPalette: (isEditting: boolean) => void;
+  setSavedColors: (colors: ColorProps[][]) => void;
+  setColors: (colors: ColorProps[]) => void;
+};
+
+export function useSavePalette({
+  colors,
+  edittingCurrentIndex,
+  isEditting,
+  setEdittingPalette,
+  setSavedColors,
+  setColors,
+}: SavePaletteProps): void {
+  const lockColors = colors.map((color) => ({
+    ...color,
+    isLocked: true,
+  }));
+
+  const savedLocalColors = getPalettes();
+
+  if (!savedLocalColors) {
+    setPalette([lockColors]);
+
+    return;
+  }
+
+  let newSavedColors: ColorProps[][] = [];
+
+  if (isEditting) {
+    newSavedColors = savedLocalColors.map((oldColor, index) => {
+      console.log(oldColor, index);
+      if (index === edittingCurrentIndex) {
+        return lockColors;
+      }
+
+      return oldColor;
+    });
+  } else {
+    newSavedColors = [...savedLocalColors, lockColors];
+  }
+
+  if (isEditting) {
+    setEdittingPalette(false);
+    setSavedColors(newSavedColors);
+
+    const unlockAllColors = newSavedColors.map((color) => {
+      console.log(color);
+      const unlockColors = color.map((item) => ({ ...item, isLocked: false }));
+
+      return unlockColors;
+    });
+
+    setColors(unlockAllColors[edittingCurrentIndex || 0]);
+  }
+
+  setPalette(newSavedColors);
+
+  return;
+}
